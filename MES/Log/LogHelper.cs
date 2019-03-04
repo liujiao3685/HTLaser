b86067b45fad2be4f21c18a4dc5898985da8205d
liujiao <liujiao@DESKTOP-IKER0EE>
2019-03-04 18:02:55 +0800

    追溯系统界面调整

---
+++ MES/Log/LogHelper.cs
@@ -0,0 +1,31 @@
+﻿using HslCommunication.LogNet;
+using MES;
+
+namespace ProductManage.Log
+{
+    public class LogHelper
+    {
+        public static ILogNet LogNet;
+
+        static LogHelper()
+        {
+            LogNet = Program.LogNet;
+        }
+
+        public static void WriteLog(string title, string info)
+        {
+            LogNet.WriteInfo(title, info);
+        }
+
+        public static void WriteDebugLog(string info)
+        {
+            LogNet.WriteDebug(info);
+        }
+
+        public static void WriteWarmLog(string info)
+        {
+            LogNet.WriteWarn(info);
+        }
+
+    }
+}
